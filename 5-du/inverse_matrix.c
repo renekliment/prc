33022b445787d65207f439ddae6005e23fcbe68c
René Kliment <rene@renekliment.cz>
2013-10-29 21:11:37 +0100

    making it actually work

--- 5-du/inverse_matrix.c
+++ 5-du/inverse_matrix.c
@@ -28,10 +28,40 @@ int main(void)
 		printf("Error: Matrix is not invertible!\n");
 		return -1;
 	}
+/*
+	float inv[DIM][DIM];
+	inv[0][0] = (mat[1][1]*mat[2][2] - mat[1][2]*mat[2][1]);
+	inv[0][1] = -(mat[0][1]*mat[2][2] - mat[0][2]*mat[2][1]);
+	inv[0][2] = (mat[0][1]*mat[1][2] - mat[0][2]*mat[1][1]);
+	inv[1][0] = -(mat[1][0]*mat[2][2] - mat[1][2]*mat[2][0]);
+	inv[1][1] = (mat[0][0]*mat[2][2] - mat[0][2]*mat[2][0]);
+	inv[1][2] = -(mat[0][0]*mat[1][2] - mat[0][2]*mat[1][0]);
+	inv[2][0] = (mat[1][0]*mat[2][1] - mat[1][1]*mat[2][0]);
+	inv[2][1] = -(mat[0][0]*mat[2][1] - mat[0][1]*mat[2][0]);
+	inv[2][2] = (mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0]);
+*/
+	float inv[DIM][DIM] =
+	{
+		{
+			(mat[1][1]*mat[2][2] - mat[1][2]*mat[2][1]),
+			-(mat[0][1]*mat[2][2] - mat[0][2]*mat[2][1]),
+			(mat[0][1]*mat[1][2] - mat[0][2]*mat[1][1])
+		},
+		{
+			-(mat[1][0]*mat[2][2] - mat[1][2]*mat[2][0]),
+			(mat[0][0]*mat[2][2] - mat[0][2]*mat[2][0]),
+			-(mat[0][0]*mat[1][2] - mat[0][2]*mat[1][0])
+		},
+		{
+			(mat[1][0]*mat[2][1] - mat[1][1]*mat[2][0]),
+			-(mat[0][0]*mat[2][1] - mat[0][1]*mat[2][0]),
+			(mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0])
+		}
+	};
 
 	for (i=0; i<DIM; i++) {
 		for (j=0; j<DIM; j++) {
-			printf("%f ", (1/det)*mat[j][i]);
+			printf("%f ", (1/det)*inv[i][j]);
 		}
 
 		putchar('\n');
